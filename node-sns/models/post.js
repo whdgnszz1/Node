3d2e1be4a82532308c22b3e1abfb8b5c04870978
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-20 00:23:14 +0900

    Setting: Create post Model

--- node-sns/models/post.js
+++ node-sns/models/post.js
@@ -1,13 +1,31 @@
-const Sequelize = require('sequelize')
+const Sequelize = require("sequelize");
 
 class Post extends Sequelize.Model {
-  static initiate(sequelize){
-
+  static initiate(sequelize) {
+    Post.init(
+      {
+        content: {
+          type: Sequelize.STRING(40),
+          allowNull: false,
+        },
+        img: {
+          type: Sequelize.STRING(200),
+          allowNull: true,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        paranoid: false,
+        modelName: "Post",
+        tableName: "posts",
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_general_ci'
+      });
   }
 
-  static associate(db) {
-
-  }
+  static associate(db) {}
 }
 
-module.exports = Post
+module.exports = Post;

50e25ee7c2d9961e61b99d7dbf3dabac1f1a84fd
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-20 23:22:55 +0900

    Feat: Add getMyPosts and getPostsByHashtag feature to API server

--- node-sns-api/controllers/v1.js
+++ node-sns-api/controllers/v1.js
@@ -1,4 +1,4 @@
-const { User, Domain } = require("../models");
+const { User, Domain, Post, Hashtag } = require("../models");
 const jwt = require("jsonwebtoken");
 
 exports.createToken = async (req, res, next) => {
@@ -33,20 +33,66 @@ exports.createToken = async (req, res, next) => {
     return res.json({
       code: 200,
       message: "success",
-      token
+      token,
     });
   } catch (error) {
     console.error(error);
     return res.status(500).json({
       code: 500,
-      message: '서버 에러'
-    })
+      message: "서버 에러",
+    });
   }
 };
 
 exports.tokenTest = async (req, res, next) => {
   try {
     res.json(res.locals.decoded);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+exports.getMyPosts = async (req, res, next) => {
+  try {
+    console.log(res.locals.decoded);
+    const posts = await Post.findAll({
+      where: {
+        userId: res.locals.decoded.id,
+      },
+    });
+    return res.json({
+      code: 200,
+      payload: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+exports.getPostsByHashtag = async (req, res, next) => {
+  try {
+    const hashtag = await Hashtag.findOne({
+      where: { title: req.params.title },
+    });
+    if (!hashtag) {
+      return res.status(404).json({
+        code: 404,
+        message: "검색 결과가 없습니다.",
+      });
+    }
+    const posts = await hashtag.getPosts();
+    if (posts.length === 0) {
+      return res.status(404).json({
+        code: 404,
+        message: "검색 결과가 없습니다.",
+      });
+    }
+    return res.json({
+      code: 200,
+      payload: posts,
+    });
   } catch (error) {
     console.error(error);
     next(error);
--- node-sns-api/routes/v1.js
+++ node-sns-api/routes/v1.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { verifyToken } = require("../middlewares");
-const { createToken, tokenTest } = require('../controllers/v1');
+const { createToken, tokenTest, getMyPosts, getPostsByHashtag } = require("../controllers/v1");
 
 router.post("/token", createToken);
 router.get("/test", verifyToken, tokenTest);
+
+router.get("/posts/my", verifyToken, getMyPosts);
+router.get("/posts/hashtag/:title", verifyToken, getPostsByHashtag);
 
 module.exports = router;

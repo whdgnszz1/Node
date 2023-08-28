7b962c45dd7f30dbbc0a46abf0ce6ea575e32834
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-28 12:56:34 +0900

    Setting prisma

--- node-sns-call/controllers/index.js
+++ node-sns-call/controllers/index.js
@@ -1,5 +1,29 @@
 const axios = require("axios");
 
+const URL = "http://localhost:8002/v1";
+axios.defaults.headers.origin = "http://localhost:4000";
+
+const request = async (req, api) => {
+  try {
+    if (!req.session.jwt) {
+      const tokenResult = await axios.get(`${URL}/token`, {
+        clientSecret: process.env.CLIENT_SECRET,
+      });
+      req.session.jwt = tokenResult.data.token;
+    }
+
+    return await axios.get(`${URL}${api}`, {
+      headers: { authorization: req.session.jwt },
+    });
+  } catch (error) {
+    if (error.response?.status === 419) {
+      delete req.session.jwt;
+      return request(req, api);
+    }
+    return error.response;
+  }
+};
+
 exports.test = async (req, res, next) => {
   try {
     // token이 없는 경우에만 token 발급
@@ -24,6 +48,24 @@ exports.test = async (req, res, next) => {
     if (error.response?.status === 419) {
       return res.json(error.response.data);
     }
-    return next(error);
+    throw error.response;
+  }
+};
+
+exports.getMyPosts = async (req, res, next) => {
+  try {
+    const result = await request(req, "/posts/my");
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+exports.searchByHashtag = async (req, res, next) => {
+  try {
+    const result = await request(req, `/posts/hashtag/${req.params.hashtag}`);
+  } catch (error) {
+    console.error(error);
+    next(error);
   }
 };
